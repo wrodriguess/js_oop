28d7589856e03b82a65981923aad828a78479847
William <willrodriguess77@gmail.com>
2022-06-19 10:26:50 -0300

    aula17 -Criando objeto e atribuindo a uma variavel

---
+++ aula17_criando_um_objeto/1_criando_objeto/script.js
@@ -0,0 +1,16 @@
+// Criando um objeto e atribuindo a uma variavel
+let carro = {
+    cor: 'azul',
+    portas: 4,
+    carroceria: 'sedan'
+}
+
+let pessoa = {
+    nome: 'Matheus',
+    sobrenome: 'Battisti',
+    idade: 25,
+    nacionalidade: 'Brasileiro'
+}
+
+console.log(typeof carro)
+console.log(pessoa)
